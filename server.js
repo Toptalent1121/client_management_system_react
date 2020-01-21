5159dfcd23d2a727bc9e545201095167077a97b0
toptalent1121 <toptalent1121@gmail.com>
2020-01-22 01:26:54 +0900

    Create Customer Database & Node.js DB connection settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,32 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const config = JSON.parse(data);
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    port: config.port,
+    database: config.database
+})
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([{
-            id: 1,
-            image: 'https://placeimg.com/64/64/1',
-            name: 'John',
-            birthday: '19801111',
-            gender: 'Male',
-            job: 'developer'
-          },
-          {
-            id: 2,
-            image: 'https://placeimg.com/64/64/2',
-            name: 'Anna',
-            birthday: '19801112',
-            gender: 'Female',
-            job: 'Designer'
-          },
-          {
-            id: 3,
-            image: 'https://placeimg.com/64/64/3',
-            name: 'Mike',
-            birthday: '19801113',
-            gender: 'Male',
-            job: 'engineer'
-          }
-    ]);
+    connection.query("SELECT * FROM customer", (err, rows, fields) => {
+        res.send(rows);
+    })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
